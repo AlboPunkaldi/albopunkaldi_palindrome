44ca37ccfaf64538e47e2cb6fddbfaadb2cbeb15
Alberto Pancaldi <alberto.pancaldi95@gmail.com>
2023-04-03 19:19:42 +0200

    Initialize repository

---
+++ lib/albopunkaldi_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "albopunkaldi_palindrome/version"
+
+module AlbopunkaldiPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
