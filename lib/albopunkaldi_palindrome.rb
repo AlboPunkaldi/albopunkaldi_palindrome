25aa47b8813eaf67d9d2926c638243b8589498fa
Alberto Pancaldi <alberto.pancaldi95@gmail.com>
2023-04-12 00:56:33 +0200

    Correcting empty string detector

--- lib/albopunkaldi_palindrome.rb
+++ lib/albopunkaldi_palindrome.rb
@@ -6,8 +6,11 @@ module AlboPunkaldiPalindrome
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    !processed_content.empty? &&
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
